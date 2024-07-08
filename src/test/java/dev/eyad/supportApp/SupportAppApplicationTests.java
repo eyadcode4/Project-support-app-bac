c633d4420c17edcb0e8ab36a0bc95fcb72359814
eyadcode24 <eyadcode24@gmail.com>
2024-07-08 14:13:34 +0200

    init

---
+++ src/test/java/dev/eyad/supportApp/SupportAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.eyad.supportApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
